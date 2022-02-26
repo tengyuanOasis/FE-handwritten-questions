8bd6740e27eb6160276dbb25f3143a2ce5f0aa8f
June <junetengma@163.com>
2022-02-26 20:36:48 +0800

    update: 更新一下代码

--- 继承/test.js
+++ 继承/test.js
@@ -18,26 +18,28 @@ Fn.prototype.getX = function () {
 	console.log(this.x);
 };
 Fn.prototype.getY = function () {
+	console.log(this)
 	console.log(this.y);
 };
 
 let f1 = new Fn();
 let f2 = new Fn();
-
-let f3 = new Fn();
-f3.prototype = new Fn2(); // ---> f3 还能不能调用到 fn 里面的属性和方法
-console.log('f3: ', f3);
-
-console.log(f1.getX === f2.getX); 
-console.log(f1.getY === f2.getY); 
-console.log(f1.__proto__.getY === Fn.prototype.getY); 
-console.log(f1.__proto__.getX === f2.getX); 
-console.log(f1.getX === Fn.prototype.getX); 
-console.log(f1.constructor); 
-console.log(Fn.prototype.__proto__.constructor); 
-f1.getX(); 
-f1.__proto__.getX(); 
 f2.getY(); 
-Fn.prototype.getY(); 
+
+// let f3 = new Fn();
+// f3.prototype = new Fn2(); // ---> f3 还能不能调用到 fn 里面的属性和方法
+// console.log('f3: ', f3);
+
+// console.log(f1.getX === f2.getX); 
+// console.log(f1.getY === f2.getY); 
+// console.log(f1.__proto__.getY === Fn.prototype.getY); 
+// console.log(f1.__proto__.getX === f2.getX); 
+// console.log(f1.getX === Fn.prototype.getX); 
+// console.log(f1.constructor); 
+// console.log(Fn.prototype.__proto__.constructor); 
+// f1.getX(); 
+// f1.__proto__.getX(); 
+// f2.getY(); 
+// Fn.prototype.getY(); 
 
 
